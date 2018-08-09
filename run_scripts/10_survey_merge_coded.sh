3a175031e6b92d1134535a2e6b96a491f91644b8
Alexander Simpson <as2388@cam.ac.uk>
2018-08-09 11:42:19 +0100

    Add pipeline stage for applying codes from Coda files to the surveys dataset.

---
+++ run_scripts/10_survey_merge_coded.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "Usage: sh 10_survey_merge_coded.sh <user> <data-root>"
+    echo "Applies codes from a Coda file to survey responses"
+    exit
+fi
+
+USER=$1
+DATA_ROOT=$2
+
+cd ../survey_merge_coded
+
+mkdir -p "$DATA_ROOT/10 Manually Coded"
+
+sh docker-run.sh "$USER" "$DATA_ROOT/05 Auto-Coded/surveys.json" \
+    "$DATA_ROOT/09 Coded Coda Files" "$DATA_ROOT/10 Manually Coded/surveys.json"
