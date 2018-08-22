2e3c41e311929e29599e7cf56cb10db4ac723d26
Alexander Simpson <as2388@cam.ac.uk>
2018-08-22 16:46:22 +0300

    Import yes/no labels from coded show Coda file

--- messages_merge_coded/messages_merge_coded.py
+++ messages_merge_coded/messages_merge_coded.py
@@ -23,16 +23,23 @@ if __name__ == "__main__":
     with open(json_input_path, "r") as f:
         show_messages = TracedDataJsonIO.import_json_to_traced_data_iterable(f)
 
-    show = 1  # TODO: Move to command line arg
+    show_number = 1  # TODO: Move to command line arg
 
-    # Merge manually coded Coda files into the cleaned dataset
+    if show_number == 1:
+        key_of_raw = "S06E01_Risk_Perception (Text) - wt_s06e1_activation"
+        key_of_coded_prefix = "{}_coded_".format(key_of_raw)
+    # TODO: Configure for other shows
+    else:
+        assert False, "Unrecognised show '{}'. Show should be a number from 1-5 inclusive.".format(show_number)
+
+    # Merge yes/no responses from the manually coded Coda files into the cleaned dataset
     with open(coded_input_path, "r") as f:
-        if show == 1:
-            key_of_raw = "S06E01_Risk_Perception (Text) - wt_s06e1_activation"
-            key_of_coded_prefix = "{}_coded_".format(key_of_raw)
-        else:
-            assert False, "Unrecognised show '{}'. Show should be a number from 1-5 inclusive.".format(show)
+        TracedDataCodaIO.import_coda_to_traced_data_iterable(
+            user, show_messages, key_of_raw, {"Yes/No": "{}yes_no".format(key_of_coded_prefix)}, f,
+            overwrite_existing_codes=True)
 
+    # Merge matrix data from the manually coded Coda files into the cleaned dataset
+    with open(coded_input_path, "r") as f:
         TracedDataCodaIO.import_coda_to_traced_data_iterable_as_matrix(
             user, show_messages, key_of_raw, {"Reason", "Reason 2"}, f, key_of_coded_prefix=key_of_coded_prefix)
         
