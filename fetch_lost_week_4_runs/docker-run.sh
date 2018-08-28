91d059b4cdb29dd07a2b27799f11794c0ce99f6b
Alexander Simpson <as2388@cam.ac.uk>
2018-08-28 11:26:25 +0300

    Fetch lost week 4 messages and append to the normal week 4 dataset

--- fetch_lost_week_4_runs/docker-run.sh
+++ fetch_lost_week_4_runs/docker-run.sh
@@ -2,28 +2,26 @@
 
 set -e
 
-IMAGE_NAME=rapid-pro-fetch-runs
+IMAGE_NAME=wt-fetch-lost-week-4-runs
 
 # Check that the correct number of arguments were provided.
-if [ $# -ne 7 ]; then
-    echo "Usage: sh docker-run.sh <server> <token> <flow-name> <user> {all,latest-only} <phone-uuid-table> <output-json>"
+if [ $# -ne 5 ]; then
+    echo "Usage: sh docker-run.sh <server> <token> <user> <phone-uuid-table> <json>"
     exit
 fi
 
 # Assign the program arguments to bash variables.
 SERVER=$1
 TOKEN=$2
-FLOW_NAME=$3
-USER=$4
-MODE=$5
-PHONE_UUID_TABLE=$6
-OUTPUT_JSON=$7
+USER=$3
+PHONE_UUID_TABLE=$4
+JSON=$5
 
 # Build an image for this pipeline stage.
 docker build -t "$IMAGE_NAME" .
 
 # Create a container from the image that was just built.
-container="$(docker container create --env SERVER="$SERVER" --env TOKEN="$TOKEN" --env FLOW_NAME="$FLOW_NAME" --env USER="$USER" --env MODE="$MODE" "$IMAGE_NAME")"
+container="$(docker container create --env SERVER="$SERVER" --env TOKEN="$TOKEN" --env USER="$USER" "$IMAGE_NAME")"
 
 function finish {
     # Tear down the container when done.
@@ -33,6 +31,7 @@ trap finish EXIT
 
 # Copy input data into the container
 docker cp "$PHONE_UUID_TABLE" "$container:/data/phone-uuid-table.json"
+docker cp "$JSON" "$container:/data/data.json"
 
 # Run the image as a container.
 docker start -a -i "$container"
@@ -41,5 +40,5 @@ docker start -a -i "$container"
 mkdir -p "$(dirname "$PHONE_UUID_TABLE")"
 docker cp "$container:/data/phone-uuid-table.json" "$PHONE_UUID_TABLE"
 
-mkdir -p "$(dirname "$OUTPUT_JSON")"
-docker cp "$container:/data/output.json" "$OUTPUT_JSON"
+mkdir -p "$(dirname "$JSON")"
+docker cp "$container:/data/data.json" "$JSON"
---
+++ fetch_lost_week_4_runs/fetch_lost_week_4_runs.py
@@ -0,0 +1,123 @@
+import argparse
+import os
+import time
+
+from core_data_modules.traced_data import TracedData, Metadata
+from core_data_modules.traced_data.io import TracedDataJsonIO
+from core_data_modules.util import PhoneNumberUuidTable
+from dateutil.parser import isoparse
+from temba_client.v2 import TembaClient
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Downloads the lost week 4 messages from Rapid Pro, and adds "
+                                                 "them to the list of existing exported messages")
+    parser.add_argument("--server", help="Address of RapidPro server. Defaults to http://localhost:8000.",
+                        nargs="?", default="http://localhost:8000")
+    parser.add_argument("token", help="RapidPro API Token")
+    parser.add_argument("user", help="Identifier of user launching this program, for use in TracedData Metadata")
+    parser.add_argument("phone_uuid_table_path", metavar="phone-uuid-table-path",
+                        help="JSON file containing an existing phone number <-> UUID lookup table. "
+                             "This file will be updated with the new phone numbers which are found by this process")
+    parser.add_argument("json_path", metavar="json-path",
+                        help="Path to serialized TracedData JSON file containing a list of week 4 runs to be "
+                             "updated. The update")
+
+    args = parser.parse_args()
+    server = args.server
+    token = args.token
+    user = args.user
+    phone_uuid_path = args.phone_uuid_table_path
+    json_path = args.json_path
+
+    rapid_pro = TembaClient(server, token)
+
+    # Load the existing phone number <-> UUID table.
+    if not os.path.exists(phone_uuid_path):
+        raise FileNotFoundError("No such phone uuid table file '{}'. "
+                                "To create a new, empty UUID table, "
+                                "run $ echo \"{{}}\" > <target-json-file>".format(phone_uuid_path))
+    with open(phone_uuid_path, "r") as f:
+        phone_uuids = PhoneNumberUuidTable.load(f)
+
+    # Load the existing messages for week 4
+    with open(json_path, "r") as f:
+        week_4_messages = TracedDataJsonIO.import_json_to_traced_data_iterable(f)
+
+    # Fetch messages in the S06e04 resend group
+    lost_messages = rapid_pro.get_messages(label="S06e04_resend").all(retry_on_rate_exceed=True)
+    print("Number of lost messages found: {}".format(len(lost_messages)))
+
+    # Sort by ascending order of modification date.
+    lost_messages = list(lost_messages)
+    lost_messages.reverse()
+
+    # Convert the lost messages to de-identified TracedData
+    traced_messages = []
+    for message in lost_messages:
+        traced_messages.append(
+            TracedData(
+                {
+                    "avf_phone_id": phone_uuids.add_phone(message.urn),
+                    "id": message.id,
+                    "text": message.text,
+                    "created_on": message.created_on.isoformat(),
+                    "sent_on": message.sent_on.isoformat(),
+                    "modified_on": message.modified_on.isoformat()
+                },
+                Metadata(user, Metadata.get_call_location(), time.time())
+            )
+        )
+
+    # Make the traced messages look like week 4 runs
+    def format_label(parameter):
+        """
+        Creates a week 4 key for the given parameter.
+
+        >>> format_label("Text")
+        'S06E04_Cholera_Recurrency (Text) - wt_s06e04_activation'
+        """
+        category_title = "S06E04_Cholera_Recurrency"
+        flow_name = "wt_s06e04_activation"
+        return "{} ({}) - {}".format(category_title, parameter, flow_name)
+
+    for traced_message in traced_messages:
+        traced_message.append_data(
+            {
+                format_label("Category"): "All Responses",
+                format_label("Value"): traced_message["text"],
+                format_label("Text"): traced_message["text"],
+                format_label("Name"): "s06e04_cholera_recurrency",
+                format_label("Time"): traced_message["sent_on"],
+                format_label("Run ID"): traced_message["id"],
+                
+                "exited_on": None,
+                "exit_type": "completed"
+            },
+            Metadata(user, Metadata.get_call_location(), time.time())
+        )
+
+    # Merge the traced runs with previous data, ignoring messages which have already been included.
+    def week_4_messages_contains_message(traced_message):
+        for message in week_4_messages:
+            if format_label("Text") in message and \
+                    message[format_label("Text")] == traced_message[format_label("Text")] and \
+                    message["avf_phone_id"] == traced_message["avf_phone_id"]:
+                return True
+        return False
+
+    added_messages_count = 0
+    for traced_message in traced_messages:
+        if not week_4_messages_contains_message(traced_message):
+            week_4_messages.append(traced_message)
+            added_messages_count += 1
+    print("Added {} messages".format(added_messages_count))
+
+    # Write the UUIDs out to a file
+    with open(phone_uuid_path, "w") as f:
+        phone_uuids.dump(f)
+
+    # Output the modified week_4_messages TracedData to JSON.
+    if os.path.dirname(json_path) is not "" and not os.path.exists(os.path.dirname(json_path)):
+        os.makedirs(os.path.dirname(json_path))
+    with open(json_path, "w") as f:
+        TracedDataJsonIO.export_traced_data_iterable_to_json(week_4_messages, f, pretty_print=True)
